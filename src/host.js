7791f3958a5543a5849f1593faca9bccdecb4add
lioenel <kepin91@yahoo.co.id>
2016-11-18 16:15:32 +0700

    change host

--- src/host.js
+++ src/host.js
@@ -1,4 +1,15 @@
 module.exports = { 
-    core: "http://dl-core-api-uat.mybluemix.net",
-    auth: "http://dl-auth-api-uat.mybluemix.net"
+    //DEV
+    core: "http://bateeq-core-api-dev.mybluemix.net/v1/core",
+    auth: "http://bateeq-auth-api-dev.mybluemix.net",
+    inventory: "http://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
+    merchandiser:"http://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
+    master: "http://bateeq-pos-api-dev.mybluemix.net/v1/master",
+    sales: "http://bateeq-pos-api-dev.mybluemix.net/v1/sales"
+   
+    //Local
+    // inventory: "http://localhost:9070/v1/inventory",
+    // merchandiser:"http://localhost:9070/v1/merchandiser",
+    // master: "http://localhost:9080/v1/master",
+    // sales: "http://localhost:9080/v1/sales"
 }
