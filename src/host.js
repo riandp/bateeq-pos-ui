03283c77826cffb4d8b3c14e3a9f0ecceac435f9
lioenel <kepin91@yahoo.co.id>
2016-11-23 10:27:44 +0700

    update host

--- src/host.js
+++ src/host.js
@@ -4,12 +4,12 @@ module.exports = {
     auth: "http://bateeq-auth-api-dev.mybluemix.net",
     inventory: "http://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
     merchandiser:"http://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
-    // master: "http://bateeq-pos-api-dev.mybluemix.net/v1/master",
-    // sales: "http://bateeq-pos-api-dev.mybluemix.net/v1/sales"
+    master: "http://bateeq-pos-api-dev.mybluemix.net/v1/master",
+    sales: "http://bateeq-pos-api-dev.mybluemix.net/v1/sales"
    
     //Local
     // inventory: "http://localhost:9070/v1/inventory",
     // merchandiser:"http://localhost:9070/v1/merchandiser",
-     master: "http://localhost:9080/v1/master",
-     sales: "http://localhost:9080/v1/sales"
+    // master: "http://localhost:9080/v1/master",
+    // sales: "http://localhost:9080/v1/sales"
 }
